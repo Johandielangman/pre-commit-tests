44a37db91ca96e8103608e6463a9954401ef43dd
Johandielangman <jghanekom2@gmail.com>
2025-04-03 21:04:50 +0200

    feat: new main file

---
+++ main.py
@@ -0,0 +1,13 @@
+def foo():
+    x = 1 + 2
+    # y = 3
+    if x > 0:
+        print("Hello")
+        print("World")
+
+
+def bar():
+    # a = list(range(1, 31))
+    # b = "some string"
+    # c =  42
+    print(foo(), bar())
